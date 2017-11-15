b3aac751a5a6890082db01dbcd62b48760be0f0c
Tony Waters <tw_ld@npd.learndirect.com>
2017-11-15 22:46:06 +0000

    Tidy up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-# group setup for running docker as jenkins from docker container
+# group setup for running docker as jenkins from docker container (need to share the same docker GID)
 groupdel docker
 DOCKER_SOCKET=/var/run/docker.sock
 DOCKER_GROUP=docker
@@ -11,11 +11,11 @@ if [ -S ${DOCKER_SOCKET} ]; then
 fi
 
 # add new files to jenkins_home
-#mv /tmp/copy/* /tmp/copy/.* /var/jenkins_home/
 cp -R /tmp/copy/ /var/jenkins_home
 cp -R /tmp/copy/.docker /var/jenkins_home
 chown -R jenkins:jenkins /var/jenkins_home
 
+# run jenkins start script
 chmod +x /usr/local/bin/jenkins.sh
 exec su jenkins -c "/usr/local/bin/jenkins.sh"
 
